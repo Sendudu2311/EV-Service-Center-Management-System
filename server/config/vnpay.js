0b6366dc06da6c45f54c868d09233006d6d3bf35
khoatq201 <khoakhoatran20.m@gmail.com>
2025-11-11 20:54:00 +0700

    test

--- server/config/vnpay.js
+++ server/config/vnpay.js
@@ -1,15 +1,19 @@
-import { VNPay } from 'vnpay';
-import dotenv from 'dotenv';
+import { VNPay } from "vnpay";
+import dotenv from "dotenv";
 
 dotenv.config();
 
 // VNPay configuration
 const vnpayConfig = {
-  tmnCode: process.env.VNP_TMNCODE || process.env.VNPAY_TMN_CODE || '',
-  secureSecret: process.env.VNP_HASH_SECRET || process.env.VNPAY_SECURE_SECRET || '',
-  vnpayHost: process.env.VNP_URL || process.env.VNPAY_HOST || 'https://sandbox.vnpayment.vn/paymentv2/vpcpay.html',
-  hashAlgorithm: 'sha512',
-  testMode: process.env.NODE_ENV === 'development'
+  tmnCode: process.env.VNP_TMNCODE || process.env.VNPAY_TMN_CODE || "",
+  secureSecret:
+    process.env.VNP_HASH_SECRET || process.env.VNPAY_SECURE_SECRET || "",
+  vnpayHost:
+    process.env.VNP_URL ||
+    process.env.VNPAY_HOST ||
+    "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html",
+  hashAlgorithm: "sha512",
+  testMode: true,
 };
 
 // Create VNPay instance
